2b7f522c88c4afe628ef62edc0bf2304efb863f3
Lise Ekhorn Johansen <liseej@uio.no>
2024-09-13 17:06:06 +0200

    removed obsolete junk

--- src/main/java/com/ass1/client/Client.java
+++ src/main/java/com/ass1/client/Client.java
@@ -1,11 +1,8 @@
 package com.ass1.client;
-
-import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 import com.ass1.server.ServerInterface;
 
@@ -15,7 +12,6 @@ public class Client {
 			Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1234);
 			ServerInterface server = (ServerInterface) registry.lookup("server");
 			System.out.println("asking server...");
-			System.out.println(server.Add(10, 20));
 			System.out.println("done");
 		} catch (RemoteException | NotBoundException e) {
 			e.printStackTrace();
