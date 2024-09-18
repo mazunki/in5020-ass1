8590a689131bcea51620a587e2e8d53dbc5bf2aa
Mazunki Hoksaas <rolferen@gmail.com>
2024-09-18 14:56:29 +0200

    move fake delay to actually used function

--- src/main/java/com/ass1/server/Server.java
+++ src/main/java/com/ass1/server/Server.java
@@ -43,15 +43,15 @@ public class Server implements ServerInterface {
 	}
 
 	public int getPopulationOfCountry(String[] args) throws RemoteException {
+		return this.getPopulationOfCountry(args[0]);
+	}
+
+	public int getPopulationOfCountry(String countryName) throws RemoteException {
 		try {
 			Thread.sleep(250); // real slow funciton fr fr no cap
 		} catch (InterruptedException e) {
 			System.err.println("uhh what");
 		}
-		return this.getPopulationOfCountry(args[0]);
-	}
-
-	public int getPopulationOfCountry(String countryName) throws RemoteException {
 		int population = 0;
 
 		// Use the GeonameLoader to fetch cities for the given country name
