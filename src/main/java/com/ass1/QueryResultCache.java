98c6fe00dd2965357e6284b082c343d9346ef1d4
mozaidan <s366611@oslomet.no>
2024-09-18 18:34:45 +0200

    change generic to object

--- src/main/java/com/ass1/QueryResultCache.java
+++ src/main/java/com/ass1/QueryResultCache.java
@@ -8,55 +8,65 @@ import java.util.Map;
 public class QueryResultCache {
 
     public static final int DEFAULT_SERVER_CACHE_LIMIT = 150;
-    public static final int DEFAULT_CLIENT_CACHE_LIMIT = 45;
+    public static final int DEFAULT_CLIENT_CACHE_LIMIT = 10;
     int size;
     LinkedHashMap<String, Object> cache;
 
     public QueryResultCache(int size) {
         QueryResultCache self = this;
         this.size = size;
-        this.cache = new LinkedHashMap<String, Object>(this.size, 0.75f, true) {
+        this.cache = new LinkedHashMap<String, Object>(this.size) {
             @Override
             protected boolean removeEldestEntry(Map.Entry<String, Object> eldest) {
                 boolean should_del = size() > self.size;
                 if (should_del) {
                     System.out.println("brr from cache");
                 }
-                return size() > self.size;
+                return should_del;
             }
         };
     }
 
     public boolean has(String method, Object[] args) {
-        return this.cache.containsKey(this.cacheKeyGenerator(method, args));
+        String key = QueryResultCache.cacheKeyGenerator(method, args);
+        boolean found = this.cache.containsKey(key);
+        if (found) {
+            System.out.println("[cache] has key: " + key);
+        }
+        return found;
     }
     public boolean has(Method method, Object[] args) {
-        return this.cache.containsKey(this.cacheKeyGenerator(method, args));
+        String key = QueryResultCache.cacheKeyGenerator(method, args);
+        boolean found = this.cache.containsKey(key);
+        if (found) {
+            System.out.println("[cache] has key: " + key);
+        }
+        return found;
     }
-    public <T> T get(String method, Object[] args) {
-        String key = this.cacheKeyGenerator(method, args);
+    public Object get(String method, Object[] args) {
+        String key = QueryResultCache.cacheKeyGenerator(method, args);
         Object result = this.cache.get(key);
         System.out.println("[cache] restoring " + key + " as " + result);
 
-        return (T) result;
+        return result;
     }
-    public <T> T get(Method method, Object[] args) {
-        String key = this.cacheKeyGenerator(method, args);
+    public Object get(Method method, Object[] args) {
+        String key = QueryResultCache.cacheKeyGenerator(method, args);
         Object result = this.cache.get(key);
         System.out.println("[cache] restoring " + key + " as " + result);
 
-        return (T) result;
+        return result;
     }
 
     public Object remember(Method method, Object[] args, Object value) {
-        String key = this.cacheKeyGenerator(method, args);
+        String key = QueryResultCache.cacheKeyGenerator(method, args);
         System.out.println("[cache] remembering " + key + " as " + value);
         this.cache.put(key, value);
         return value;
     }
 
     public Object remember(String method, Object[] args, Object value) {
-        String key = this.cacheKeyGenerator(method, args);
+        String key = QueryResultCache.cacheKeyGenerator(method, args);
         System.out.println("[cache] remembering " + key + " as " + value);
         this.cache.put(key, value);
         return value;
