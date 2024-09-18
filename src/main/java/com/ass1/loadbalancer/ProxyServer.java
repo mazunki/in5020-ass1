7155382a04c58a1ce634d5b634c25614c6681ce5
Lise Ekhorn Johansen <liseej@uio.no>
2024-09-18 16:01:22 +0200

    cleanup

--- src/main/java/com/ass1/loadbalancer/ProxyServer.java
+++ src/main/java/com/ass1/loadbalancer/ProxyServer.java
@@ -7,7 +7,6 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
